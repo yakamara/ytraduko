056ca63e5641a3cc9b816811e4ae58c32a3239d5
Gregor Harlan <mail@gh01.de>
2020-01-24 23:26:34 +0100

    Spaltentitel für Mobilansicht

--- fragments/ytraduko/overview.php
+++ fragments/ytraduko/overview.php
@@ -16,10 +16,10 @@
             <td class="rex-table-icon"></td>
             <td><b><?= mb_strtoupper($this->i18n('ytraduko_total')) ?></b></td>
             <td></td>
-            <td class="text-center"><b><?= $this->total['de_de'] ?></b></td>
+            <td class="text-center" data-title="de_de"><b><?= $this->total['de_de'] ?></b></td>
             <?php foreach ($this->languages as $language): ?>
                 <?php $percentage = (int) (100 * $this->total[$language] / $this->total['de_de']); ?>
-                <td class="text-center">
+                <td class="text-center" data-title="<?= $this->escape($language) ?>">
                     <div class="progress" style="margin-bottom: 0" title="<?= $this->total[$language], ' / ', $this->total['de_de'] ?>">
                         <div class="progress-bar progress-bar-success" style="width: <?= $percentage ?>%">
                             <?= $percentage ?> %
@@ -39,12 +39,12 @@
             <?php foreach ($groupPackages as $package): /** @var rex_ytraduko_package $package */ ?>
                 <tr>
                     <td class="rex-table-icon"><i class="rex-icon rex-icon-package-addon"></i></td>
-                    <td><?= $this->escape($package->getName()) ?></td>
-                    <td><?= $this->escape($package->getVersion()) ?></td>
-                    <td class="text-center"><?= $package->countKeys() ?></td>
+                    <td data-title="<?= rex_i18n::msg('package_hname') ?>"><?= $this->escape($package->getName()) ?></td>
+                    <td data-title="<?= rex_i18n::msg('package_hversion') ?>"><?= $this->escape($package->getVersion()) ?></td>
+                    <td class="text-center" data-title="de_de"><?= $package->countKeys() ?></td>
                     <?php foreach ($this->languages as $language): ?>
                         <?php $percentage = (int) (100 * $package->countLanguageKeys($language) / $package->countKeys()); ?>
-                        <td class="text-center">
+                        <td class="text-center" data-title="<?= $this->escape($language) ?>">
                             <a href="<?= $this->context->getUrl(['language' => $language, 'package' => $package->getName()]) ?>">
                                 <div class="progress" style="margin-bottom: 0" title="<?= $package->countLanguageKeys($language), ' / ', $package->countKeys() ?>">
                                     <div class="progress-bar progress-bar-success" style="width: <?= $percentage ?>%">
